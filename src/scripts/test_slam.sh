41b9ff141ef132e40af8c5e2f829415ab3239c61
root <boli.ufl@gmail.com>
2020-11-09 03:20:03 +0000

    Move script files.

--- src/scripts/test_slam.sh
+++ src/scripts/test_slam.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_world.launch " &
+xterm  -e  " catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch " &
 sleep 5
-xterm  -e  " source devel/setup.bash; roslaunch gmapping_demo.launch" &
+xterm  -e  " catkin_make; source devel/setup.bash; roslaunch turtlebot_navigation gmapping_demo.launch " &
 sleep 5
-xterm  -e  " source devel/setup.bash; roslaunch view_navigation.launch" &
+xterm  -e  " catkin_make; source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation_with_marker.launch " &
 sleep 5
-xterm  -e  " source devel/setup.bash; roslaunch keyboard_teleop.launch"
+xterm  -e  " catkin_make; source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch "

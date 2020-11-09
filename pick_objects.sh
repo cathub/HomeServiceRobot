0e3f9eeb32a819d9db758d359697d516a68c9c52
root <boli.ufl@gmail.com>
2020-11-09 03:23:56 +0000

    Delete unnecessary files.

--- pick_objects.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xterm  -e  " catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch " &
-sleep 5
-xterm  -e  " catkin_make; source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch " & 
-sleep 5
-xterm  -e  " catkin_make; source devel/setup.bash; roslaunch pick_objects simple_pick_objects.launch "
